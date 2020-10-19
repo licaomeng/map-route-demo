b28be48f51d7e60e66a684d70ebb5dde2db4c57d
Messi Li <1967432704@qq.com>
2020-10-19 14:29:27 +0800

    feat: custom fetch
    1.  add retry logic
    2. refine logic for status code

--- src/utils/fetch/index.jsx
+++ src/utils/fetch/index.jsx
@@ -2,19 +2,37 @@
  * Customized Fetch
  * wrapped the `window.fetch()`, unify the data format and the error handling
  * 
- * @param  {...any} args same as the arguments `window.fetch()`
+ * "in progress": retry
+ * "failure": error msg
+ * "success": display data
+ * 
+ * @param  {...any} args same as the arguments `window.fetch()`, also append retry times
  */
 async function Fetch(...args) {
-  const res = await fetch(...args)
+  let [url, options, times = 0] = args
+  if (times >= 5) {
+    // retry times limited to 5
+    return Promise.reject('request time out!')
+  }
+  const res = await fetch(url, options)
   if (res.ok) {
+    // succeeded
     let json = await res.json();
     const { status, error, ...others } = json
     if (status === 'failure') {
       return Promise.reject(error)
     }
+    if (status === 'in progress') {
+      // retry logic
+      return Fetch(url, options, ++times)
+    }
     return Promise.resolve(others)
+  } else if(res.status === 500) {
+    // 500: Internal Server Error
+    return Promise.reject('Internal Server Error')
   } else {
-    return Promise.reject('error')
+    // other codes
+    return Promise.reject('request failed')
   }
 }
 
