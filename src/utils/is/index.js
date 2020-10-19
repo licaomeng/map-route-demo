8ae1486ecc4fb47c2b1aebf790fa64c387b136e1
Messi Li <1967432704@qq.com>
2020-10-19 22:51:13 +0800

    fix: util isEmptyObj
    consider `undefined`

--- src/utils/is/index.js
+++ src/utils/is/index.js
@@ -1,4 +1,4 @@
-const isEmptyObj = obj => obj.constructor === Object && Object.entries(obj).length === 0
+const isEmptyObj = obj => obj !== void 0 && obj.constructor === Object && Object.entries(obj).length === 0
 const isObj = obj => obj !== void 0 && obj.constructor === Object
 
 export {
