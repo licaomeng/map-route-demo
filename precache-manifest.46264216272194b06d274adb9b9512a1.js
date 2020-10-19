a64ce823563f1aff33e74cb2e3d3a7b04bfbb191
Messi Li <1967432704@qq.com>
2020-10-20 03:40:20 +0800

    Updates

---
+++ precache-manifest.46264216272194b06d274adb9b9512a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dabe2a066f71e996ac7c1373a38b16d",
+    "url": "/map-route-demo/index.html"
+  },
+  {
+    "revision": "1347944ee23f378b455e",
+    "url": "/map-route-demo/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "ebffd176de89092c8426",
+    "url": "/map-route-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1347944ee23f378b455e",
+    "url": "/map-route-demo/static/js/2.ff2b7348.chunk.js"
+  },
+  {
+    "revision": "8d7253e2bb4c7904fd9fcb45651a7d90",
+    "url": "/map-route-demo/static/js/2.ff2b7348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebffd176de89092c8426",
+    "url": "/map-route-demo/static/js/main.c763e9dc.chunk.js"
+  },
+  {
+    "revision": "86273ae72d3b29646099",
+    "url": "/map-route-demo/static/js/runtime-main.4b1aec3e.js"
+  }
+]);
